bd9a248e80dfbe5993e6b13aa81a5adbde67f426
Jeanpierre Saldivar <50430920+JeanpierreSaldivar@users.noreply.github.com>
2020-10-15 18:19:59 -0500

    18_19

--- app/src/main/java/com/saldivar/certifgood/CertificacionesActivity.kt
+++ app/src/main/java/com/saldivar/certifgood/CertificacionesActivity.kt
@@ -1,5 +1,6 @@
 package com.saldivar.certifgood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class CertificacionesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_certificaciones)
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        overridePendingTransition(R.anim.right_in, R.anim.right_out)
+        finish()
     }
 }
--- app/src/main/java/com/saldivar/certifgood/repo/Repo.kt
+++ app/src/main/java/com/saldivar/certifgood/repo/Repo.kt
@@ -1,18 +1,33 @@
 package com.saldivar.certifgood.repo
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.saldivar.certifgood.CredentialesLogin
 
 class Repo {
+    val dbFirestore = FirebaseFirestore.getInstance()
     fun getCredenciales():LiveData<Boolean>{
         val mutableResponse = MutableLiveData<Boolean>()
-        FirebaseFirestore.getInstance().collection("USUARIOS").whereEqualTo("user", CredentialesLogin.usuario)
+        dbFirestore.collection("USUARIOS").whereEqualTo("user", CredentialesLogin.usuario)
             .whereEqualTo("password", CredentialesLogin.password).get().addOnSuccessListener {
+                for (document in it){
+                    val idDocumento=document.id
+                    updateActividadUser(idDocumento)
+                }
+
                 mutableResponse.value = it.size() != 0
             }
         return mutableResponse
+    }
+
+    fun updateActividadUser(idDocumento: String):LiveData<Boolean>{
+        val mutableResponse = MutableLiveData<Boolean>()
+        dbFirestore.collection("USUARIOS").document(idDocumento).update("actividad_usuario",true)
+            .addOnSuccessListener {
+
+            }.addOnFailureListener {
+
+            }
     }
 }
